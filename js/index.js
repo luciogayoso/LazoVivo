8c19052a3b45cff0605a5a99a72456e17e72ac48
luciogayoso <luciogayoso@gmail.com>
2025-08-16 17:11:26 -0300

    contacto y footer

--- js/index.js
+++ js/index.js
@@ -3,11 +3,17 @@ var ticking = false;
 
 function doSomething(scroll_pos) {
     let nav = document.querySelector("#nav");
+    var btn = document.getElementById("btn-volver-arriba");
+    if (scroll_pos > 20 || document.documentElement.scrollTop > 20) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
     if (scroll_pos > 60) {
-        nav.classList.remove("bg-opacity-10")
+        nav.classList.remove("bg-opacity-25");
     }
     if (scroll_pos < 60) {
-        nav.classList.add("bg-opacity-10")
+        nav.classList.add("bg-opacity-25");
     }
 }
 
@@ -20,4 +26,9 @@ window.addEventListener("scroll", function (e) {
     });
   }
   ticking = true;
+});
+
+document.getElementById("btn-volver-arriba").addEventListener("click", function() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
 });
